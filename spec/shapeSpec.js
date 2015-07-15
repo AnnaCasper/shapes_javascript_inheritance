4f29db79a84948f1215017234ca67631eff2ad56
Anna Casper <anna.casper3@gmail.com>
2015-07-15 17:41:06 -0600

    all shape specs are passing

--- spec/shapeSpec.js
+++ spec/shapeSpec.js
@@ -16,5 +16,28 @@ describe("Shape", function() {
     });
   });
 
-  // Write more specs!!
+  describe("Perimeter", function(){
+    it('should be 0 for this abstract shape', function(){
+      expect(shape.perimeter()).toEqual(0);
+    });
+  });
+
+  describe('Draw', function(){
+    it('should return a string specifies the number of sides', function(){
+      expect(shape.draw()).toEqual('A shape with 1 sides');
+    });
+  });
+
+  describe('toString', function(){
+    it('should return a string that specifies the number of sides and the color', function(){
+      expect(shape.toString()).toEqual('[Shape sides:1, color:red ]')
+    });
+  });
+
+  describe('getRGB', function(){
+    it('should return rgb value as a string', function(){
+      expect(shape.getRGB()).toEqual('rgb(255,0,0)');
+    });
+  });
+  
 });
